7464973d01d31fa2ec11039a087b813c82a961e1
Sa Sh <s@test.com>
2024-06-20 16:14:53 +0100

    chap2: example of Client Component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,28 @@
+"use client";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const name = searchParams.get("name");
+  const pathname = usePathname();
+
+  return (
+    <div>
+      <h1 className="text-3xl">
+        Property Page - {name} - {pathname}
+      </h1>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {id} (client onclick)
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
